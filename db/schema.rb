73a1f5ce8cbcfadb0f9085ce222a7e5ea6dd44fe
xand.jpeg <125098050+xandejpeg@users.noreply.github.com>
2026-07-25 18:07:59 -0300

    Update schema for ranking override

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_12_180000) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_18_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_12_180000) do
     t.string "display_name", null: false
     t.bigint "driver_id", null: false
     t.integer "kind", default: 0, null: false
+    t.string "ranking_override"
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_driver_profiles_on_code", unique: true
     t.index ["driver_id"], name: "index_driver_profiles_on_driver_id"
